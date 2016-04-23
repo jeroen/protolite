78a27abd70dabc984df764cca37537db33e4aa5a
Jeroen Ooms <jeroenooms@gmail.com>
2016-04-23 12:53:03 +0200

    Add python tests

---
+++ python/runtest.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#write some PB data with R
+Rscript writemsg.R
+
+#compile proto file
+cp -f ../src/rexp.proto .
+protoc rexp.proto --python_out=.
+rm rexp.proto
+
+#read with python and print
+python readmsg.py
+
+#cleanup tmp files
+rm rexp_pb2.py rexp_pb2.pyc
+rm *.msg
