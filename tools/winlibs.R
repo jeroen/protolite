9db7e734a52a357660f87d83ccc7d309645248e4
Jeroen Ooms <jeroenooms@gmail.com>
2023-01-29 14:46:11 +0100

    Windows: update to protobuf 3.12

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -2,7 +2,7 @@
 VERSION <- commandArgs(TRUE)
 if (!file.exists(sprintf("../windows/protobuf-%s/include/google/protobuf/descriptor.h", VERSION))) {
     if (getRversion() < "3.3.0") setInternet2()
-    download.file(sprintf("https://github.com/rwinlib/protobuf/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
+    download.file(sprintf("https://github.com/rwinlib/protobuf/archive/%s.zip", VERSION), "lib.zip", quiet = TRUE)
     dir.create("../windows", showWarnings = FALSE)
     unzip("lib.zip", exdir = "../windows")
     unlink("lib.zip")
