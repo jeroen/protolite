628539d1ce6cb5a1d163a61de0285883b4eabc54
Jeroen Ooms <jeroenooms@gmail.com>
2023-09-25 19:16:45 +0200

    Fix clang

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -2,8 +2,9 @@ if(!file.exists("../windows/protobuf/include/google/protobuf/descriptor.h")){
   unlink("../windows", recursive = TRUE)
   url <- if(grepl("aarch", R.version$platform)){
     "https://github.com/r-windows/bundles/releases/download/protobuf-21.12/protobuf-21.12-clang-aarch64.tar.xz"
+  } else if(grepl("clang", Sys.getenv('R_COMPILED_BY'))){
+    "https://github.com/r-windows/bundles/releases/download/protobuf-21.12/protobuf-21.12-clang-x86_64.tar.xz"
   } else if(getRversion() >= "4.3") {
-    # NB: does not work with rtools42
     "https://github.com/r-windows/bundles/releases/download/protobuf-21.12/protobuf-21.12-ucrt-x86_64.tar.xz"
   } else {
     "https://github.com/rwinlib/protobuf/archive/v21.12.tar.gz"
